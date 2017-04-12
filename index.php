858df44a078046597b6ccb0dc7452e7acf5d6d86
wilmerace <wiltra056@hotmail.com>
2017-04-12 12:03:20 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="es" xmlns="http://www.w3.org/1999/html">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>ComCoin</title>
+    <link rel="shortcut icon" type="image/x-icon" href="img/logo.ico" />
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+    <!-- Custom CSS -->
+    <link href="css/lovehub.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/animate.css">
+
+    <!-- Custom Fonts -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+  </head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header animated bounceInDown">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                <i class="fa fa-bars fa-2x"></i>
+            </button>
+            <a class="navbar-brand page-scroll" href="#page-top">
+                COMCOIN<span class="color"> SOLUCIONES</span><a>TI</a>
+            </a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-right navbar-main-collapse animated bounceInDown">
+            <ul class="nav navbar-nav">
+                <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#about">QUIENES SOMOS</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#team">SERVICIOS</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- Intro Header -->
+<header class="intro">
+    <div class="intro-body">
+        <div class="container">
+            <div class="col-md-7" >
+                <h1 class="brand-heading animated slideInUp"><img class="img-responsive animated pulse infinite"  src="img/LOGO.png" width="600" ></h1>
+                <h3 class="brand-heading animated bounceInLeft">SERVICIOS T.I. CON LA MEJOR CALIDAD Y AL ALCANCE DE SU MANO</h3>
+            </div>
+            <div class="col-md-5 animated bounceInDown">
+                <ul class="list-inline lead">
+                    <li>
+                        <h1 class="btn btn-default btn-lg page-scroll animated pulse infinite">Contactos<br>Tel: 3228568760<br>3124357017<br>3103163594<br>Colombia</h1>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
+
+<!-- Countdown -->
+<section id="countdown" class="container-fluid text-center">
+    <h2 class="headerline">Soportes</h2>
+    <div class="row">
+  <div class="col-xs-6 col-sm-3"><img class="img-responsive animated pulse infinite"  src="img/micro.png" width="600" ></div>
+  <div class="col-xs-6 col-sm-3"><img class="img-responsive animated pulse infinite"  src="img/linux.png" width="600" ></div>
+  <div class="col-xs-6 col-sm-3"><img class="img-responsive animated pulse infinite"  src="img/mac.png" width="400"  ></div>
+  <div class="col-xs-6 col-sm-3"><img class="img-responsive animated pulse infinite"  src="img/android.png" width="400" ></div>
+</div>
+</section>
+
+<!-- About Section -->
+<section id="about" class="container-fluid">
+    <div class="row">
+        <div class="col-sm-6 split-padding3">
+            <h2 class="headerline">Quienes somos</h2>
+            <div class="row">
+          <div class="col-sm-4"><h1>Mision</h1></div>
+          <div class="col-sm-4"><h1>Vision</h1></div>
+          </div>
+        </div>
+    </div>
+</section>
+
+<!-- Progress Section -->
+<section id="progress" class="container-fluid text-center">
+        <div class="col-lg-6 col-lg-offset-3">
+<h1>hola</h1>
+
+        </div>
+</section>
+
+<!-- Team Section -->
+<section id="team" class="container-fluid text-center">
+
+    <div class="row">
+        <h2 class="headerline">Servicios</h2><br>
+
+        <div class="col-lg-3 col-md-6">
+            <h4 class="headerline">Mantenimiento de computadores</h4>
+            <p><img src="img/avatar1.jpg" class="img-responsive center-block" alt=""></p>
+            <p>Mantenimiento correctivo y preventivo de computadores y portátiles.</p>
+
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <h4 class="headerline">Desarrollo web</h4>
+            <p><img src="img/avatar2.jpg" class="img-responsive center-block" alt=""></p>
+            <p>Descripcion</p>
+
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <h4 class="headerline">Soporte Tecnico</h4>
+            <p><img src="img/avatar3.jpg" class="img-responsive center-block" alt=""></p>
+            <p>Formateo de discos duros e instalación de software y programas</p>
+
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <h4 class="headerline">BACKUP EN LA NUBE</h4>
+            <p><img src="img/avatar4.jpg" class="img-responsive center-block" alt=""></p>
+            <p>Sus datos resguardados 100% seguros y al alcance desde cualquier lugar con conexión a internet</p>
+
+        </div>
+
+    </div>
+</section>
+
+<!-- Contact Section -->
+<section id="contact" class="container-fluid text-center">
+    <div class="overlay"></div>
+    <div class="col-lg-6 col-lg-offset-3">
+        <h2 class="headerline">Contactos <br></h2>
+        <h2 >Telefonos:</h3>
+        <h3><br>3228568760<br>3124357017<br>3103163594</h3>
+        <h3 class="headerline">Email</h3>
+        <h3 >wiltra056@hotmail.com</h3>
+
+    </div>
+
+</section>
+
+<!-- jQuery -->
+<script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/jquery.countdown.min.js"></script>
+<script src="js/smoothscroll.js"></script>
+
+<!-- Header Background Slider -->
+<script src="js/jquery.backstretch.min.js"></script>
+<script type="text/javascript">
+    $(".intro").backstretch([
+        "img/1.jpg"
+        , "img/2.jpg"
+        , "img/3.jpg"
+        , "img/4.jpg"
+        , "img/5.jpg"
+        , "img/6.jpg"
+        , "img/7.jpg"
+    ], {duration: 4000, fade: 1000, centeredX:true, centeredY:true});
+</script>
+
+<script src="js/lovehub.js"></script>
+
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-1057679-2', 'auto');
+    ga('send', 'pageview');
+</script>
+<footer>
+  <div class="container-fluid">
+  <br>
+  <h4 align="center" class="text-muted">Copyright &copy; ComCoin Soluciones T.I. 2017</h4>
+  </div>
+  
+  <script>
+  (function (w,i,d,g,e,t,s) {w[d] = w[d]||[];t= i.createElement(g);
+  t.async=1;t.src=e;s=i.getElementsByTagName(g)[0];s.parentNode.insertBefore(t, s);
+  })(window, document, '_gscq','script','//widgets.getsitecontrol.com/80008/script.js');
+  </script>
+
+</footer>
+
+</body>
+</html>
